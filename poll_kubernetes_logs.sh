0162efdc1462d0a47b2475df9f77b570b5e055cd
Aleksey Vlasenko <alekseyv@google.com>
2020-03-26 16:34:08 -0700

    added kubernetes runner

---
+++ poll_kubernetes_logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CURRENT_DATE_UTC=`date --utc -Iseconds`
+while true; do
+echo "logs since ${CURRENT_DATE_UTC}"
+sleep 60
+gcloud logging read "resource.type=\"container\"
+resource.labels.namespace_id=\"kubernetes-runner-namespace\"
+timestamp>=\"${CURRENT_DATE_UTC}\"
+" --limit 1000000000000 --order asc --format 'value(resource.labels.pod_id, jsonPayload.message, textPayload)' > logfile.txt
+cat logfile.txt | sed '/^$/d'
+if [[ $(cat logfile.txt | head -n 5 | wc -l) -ne 0 ]]; then
+    CURRENT_DATE_UTC=`date --utc -Iseconds`;
+else
+    kubectl get events --sort-by=.metadata.creationTimestamp | tail -n 2
+fi
+
+done
